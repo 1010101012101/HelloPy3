96db59c343e403ceb397ef49b389145f360e4157
menghaocheng <menghaocheng@qq.com>
2018-11-10 10:55:02 +0800

    urllib

--- Hello/h19_urllib.py
+++ Hello/h19_urllib.py
@@ -1,6 +1,15 @@
 from urllib import request
+#
+# with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+#     data = f.read()
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', data.decode('utf-8'))
 
-with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+url = 'http://736eee.com/htm/2018/11/2/t03/422669.html'
+url = "http://736eee.com/htm/2018/11/6/t03/422984.html"
+with request.urlopen(url) as f:
     data = f.read()
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
