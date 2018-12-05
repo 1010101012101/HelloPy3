bb3559be2bed178a8465fe4dd6648a2c209e42b9
mc.meng <menghaocheng@qq.com>
2018-12-06 01:55:23 +0800

    source 4, week1

---
+++ hello_numpy/h01_pad.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+import numpy as np
+a = [1,2,3,4,5,6]
+result = np.lib.pad(a, pad_width=(2,3), mode='constant', constant_values=0)
+print(result)
+
+b = [[1, 2],
+     [3, 4]]
+
+result = np.lib.pad(b, pad_width=(2,2), mode='constant', constant_values=0)
+print(result)
+
