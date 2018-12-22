41ff1ffdb84d806da4b6fae76d375df94af3a9f8
mc.meng <menghaocheng@qq.com>
2018-12-23 02:02:28 +0800

    deeplearning.ai

--- deeplearningai/course5/week1/e1/h00_utils.py
+++ deeplearningai/course5/week1/e1/h00_utils.py
@@ -165,3 +165,53 @@ def lstm_cell_forward(xt, a_prev, c_prev, parameters):
 
     return a_next, c_next, yt_pred, cache
 
+
+
+def rnn_cell_backward(da_next, cache):
+    """
+    Implements the backward pass for the RNN-cell (single time-step).
+
+    Arguments:
+    da_next -- Gradient of loss with respect to next hidden state
+    cache -- python dictionary containing useful values (output of rnn_cell_forward())
+
+    Returns:
+    gradients -- python dictionary containing:
+                        dx -- Gradients of input data, of shape (n_x, m)
+                        da_prev -- Gradients of previous hidden state, of shape (n_a, m)
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dba -- Gradients of bias vector, of shape (n_a, 1)
+    """
+
+    # Retrieve values from cache
+    (a_next, a_prev, xt, parameters) = cache
+
+    # Retrieve values from parameters
+    Wax = parameters["Wax"]
+    Waa = parameters["Waa"]
+    Wya = parameters["Wya"]
+    ba = parameters["ba"]
+    by = parameters["by"]
+
+    ### START CODE HERE ###
+    # compute the gradient of tanh with respect to a_next (≈1 line)
+    dtanh = da_next* (1 - a_next ** 2)
+
+    # compute the gradient of the loss with respect to Wax (≈2 lines)
+    dxt = np.dot(Wax.T, dtanh)
+    dWax = np.dot(dtanh, xt.T)
+
+    # compute the gradient with respect to Waa (≈2 lines)
+    da_prev = np.dot(Waa.T, dtanh)
+    dWaa = np.dot(dtanh, a_prev.T)
+
+    # compute the gradient with respect to b (≈1 line)
+    dba = np.sum(dtanh, axis=-1, keepdims=True)
+
+    ### END CODE HERE ###
+
+    # Store the gradients in a python dictionary
+    gradients = {"dxt": dxt, "da_prev": da_prev, "dWax": dWax, "dWaa": dWaa, "dba": dba}
+
+    return gradients
