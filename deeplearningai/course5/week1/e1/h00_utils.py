7b872076d021624a4afdb5380ea97ce3d6deca35
mc.meng <menghaocheng@qq.com>
2018-12-23 13:11:09 +0800

    deeplearning.ai

--- deeplearningai/course5/week1/e1/h00_utils.py
+++ deeplearningai/course5/week1/e1/h00_utils.py
@@ -167,6 +167,70 @@ def lstm_cell_forward(xt, a_prev, c_prev, parameters):
 
 
 
+def lstm_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network using an LSTM-cell described in Figure (3).
+
+    Arguments:
+    x -- Input data for every time-step, of shape (n_x, m, T_x).
+    a0 -- Initial hidden state, of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+                        Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+                        Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        bc -- Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        bo -- Bias of the output gate, numpy array of shape (n_a, 1)
+                        Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a -- Hidden states for every time-step, numpy array of shape (n_a, m, T_x)
+    y -- Predictions for every time-step, numpy array of shape (n_y, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of all the caches, x)
+    """
+
+    # Initialize "caches", which will track the list of all the caches
+    caches = []
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from shapes of x and parameters['Wy'] (≈2 lines)
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters["Wy"].shape
+
+    # initialize "a", "c" and "y" with zeros (≈3 lines)
+    a = np.zeros([n_a, m, T_x])
+    c = np.zeros([n_a, m, T_x])
+    y = np.zeros([n_y, m, T_x])
+
+    # Initialize a_next and c_next (≈2 lines)
+    a_next = a0
+    c_next = np.zeros([n_a, m])
+
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, next memory state, compute the prediction, get the cache (≈1 line)
+        a_next, c_next, yt, cache = lstm_cell_forward(x[:, :, t], a_next, c_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:, :, t] = a_next
+        # Save the value of the prediction in y (≈1 line)
+        y[:, :, t] = yt
+        # Save the value of the next cell state (≈1 line)
+        c[:, :, t] = c_next
+        # Append the cache into caches (≈1 line)
+        caches.append(cache)
+
+    ### END CODE HERE ###
+
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+
+    return a, y, c, caches
+
+
+
 def rnn_cell_backward(da_next, cache):
     """
     Implements the backward pass for the RNN-cell (single time-step).
@@ -213,5 +277,83 @@ def rnn_cell_backward(da_next, cache):
 
     # Store the gradients in a python dictionary
     gradients = {"dxt": dxt, "da_prev": da_prev, "dWax": dWax, "dWaa": dWaa, "dba": dba}
+
+    return gradients
+
+
+
+def lstm_cell_backward(da_next, dc_next, cache):
+    """
+    Implement the backward pass for the LSTM-cell (single time-step).
+
+    Arguments:
+    da_next -- Gradients of next hidden state, of shape (n_a, m)
+    dc_next -- Gradients of next cell state, of shape (n_a, m)
+    cache -- cache storing information from the forward pass
+
+    Returns:
+    gradients -- python dictionary containing:
+                        dxt -- Gradient of input data at time-step t, of shape (n_x, m)
+                        da_prev -- Gradient w.r.t. the previous hidden state, numpy array of shape (n_a, m)
+                        dc_prev -- Gradient w.r.t. the previous memory state, of shape (n_a, m, T_x)
+                        dWf -- Gradient w.r.t. the weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        dWi -- Gradient w.r.t. the weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        dWc -- Gradient w.r.t. the weight matrix of the memory gate, numpy array of shape (n_a, n_a + n_x)
+                        dWo -- Gradient w.r.t. the weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        dbf -- Gradient w.r.t. biases of the forget gate, of shape (n_a, 1)
+                        dbi -- Gradient w.r.t. biases of the update gate, of shape (n_a, 1)
+                        dbc -- Gradient w.r.t. biases of the memory gate, of shape (n_a, 1)
+                        dbo -- Gradient w.r.t. biases of the output gate, of shape (n_a, 1)
+    """
+
+    # Retrieve information from "cache"
+    (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters) = cache
+
+    ### START CODE HERE ###
+    # Retrieve dimensions from xt's and a_next's shape (≈2 lines)
+    n_x, m = xt.shape
+    n_a, m = a_next.shape
+
+    # Compute gates related derivatives, you can find their values can be found by looking carefully at equations (7) to (10) (≈4 lines)
+    dot = da_next * np.tanh(c_next) * ot * (1 - ot)
+    dcct = (dc_next * it + ot * (1 - np.square(np.tanh(c_next))) * it * da_next) * (1 - np.square(cct))
+    dit = (dc_next * cct + ot * (1 - np.square(np.tanh(c_next))) * cct * da_next) * it * (1 - it)
+    dft = (dc_next * c_prev + ot *(1 - np.square(np.tanh(c_next))) * c_prev * da_next) * ft * (1 - ft)
+
+    #     dot  = da_next * np.tanh(c_next) * ot * (1-ot)
+    #     dcct = (dc_next * it + ot * (1 - np.square(np.tanh(c_next)))) * it * da_next * (1 - np.square(cct))
+    #     dit  = (dc_next * cct + ot * (1-np.tanh(c_next)**2))*cct*da_next*it*(1-it)
+    #     dft  = (dc_next * c_prev + ot * (1-np.tanh(c_next)**2))*c_next*da_next*ft*(1-ft)
+
+    # Code equations (7) to (10) (≈4 lines)
+    # 连接矩阵
+    concat = np.concatenate((a_prev, xt), axis=0)
+
+    # Compute parameters related derivatives. Use equations (11)-(14) (≈8 lines)
+    dWf = np.dot(dft, concat.T)
+    dWi = np.dot(dit, concat.T)
+    dWc = np.dot(dcct, concat.T)
+    dWo = np.dot(dot, concat.T)
+
+    dbf = np.sum(dft, axis=1, keepdims=True)
+    dbi = np.sum(dit, axis=1, keepdims=True)
+    dbc = np.sum(dcct, axis=1, keepdims=True)
+    dbo = np.sum(dot, axis=1, keepdims=True)
+
+    # Compute derivatives w.r.t previous hidden state, previous memory state and input. Use equations (15)-(17). (≈3 lines)
+    da_prev = np.dot(parameters['Wf'][:, :n_a].T, dft) + np.dot(parameters['Wi'][:, :n_a].T, dit) + np.dot(
+        parameters['Wc'][:, :n_a].T, dcct) + np.dot(parameters['Wo'][:, :n_a].T, dot)
+    dc_prev = dc_next * ft + ot * (1 - np.square(np.tanh(c_next))) * ft * da_next
+    dxt = np.dot(parameters['Wf'][:, n_a:].T, dft) + np.dot(parameters['Wi'][:, n_a:].T, dit) + np.dot(
+        parameters['Wc'][:, n_a:].T, dcct) + np.dot(parameters['Wo'][:, n_a:].T, dot)
+
+    #     da_prev = parameters["Wf"][:n_a,:]*dft + parameters["Wi"][:n_a,:]*dit+parameters["Wc"][:n_a,:]*dcct+parameters["Wo"][:n_a,:]*dot
+    #     dc_prev = np.dot(dc_next,ft) + it*(1-np.tanh(c_next)**2)*ft*da_next
+    #     dxt = parameters["Wf"][n_a:,:]*dft+parameters["Wi"][n_a:,:]*dit+parameters["Wc"][n_a:,:]*dcct+parameters["Wo"][n_a:,:]*dot
+    ### END CODE HERE ###
+
+    # Save gradients in dictionary
+    gradients = {"dxt": dxt, "da_prev": da_prev, "dc_prev": dc_prev, "dWf": dWf, "dbf": dbf, "dWi": dWi, "dbi": dbi,
+                 "dWc": dWc, "dbc": dbc, "dWo": dWo, "dbo": dbo}
 
     return gradients
